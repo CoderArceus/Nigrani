43370c3dec7d83225f8b606de825df904930621e
CoderArceus <ap523624@gmail.com>
2026-09-11 20:55:30 +0530

    Use native 9:16 vertical background image for sidebar

--- Nigrani/src/components/PublicSidebar.tsx
+++ Nigrani/src/components/PublicSidebar.tsx
@@ -31,7 +31,7 @@ export function PublicSidebar() {
         className="absolute inset-0 z-0 pointer-events-none"
         style={{
           backgroundImage: "url('/sidebar-bg.jpg')",
-          backgroundSize: "160% auto",
+          backgroundSize: "cover",
           backgroundPosition: "left bottom",
           backgroundRepeat: "no-repeat",
           opacity: 0.6,
